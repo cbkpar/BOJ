c0f406418b7562a850e8946fda618e9c1b27ecee
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-07-03 12:30:25 +0900

    Create boj_2703.java

---
+++ boj_2703.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int t,i;
+		t = Integer.parseInt(br.readLine());
+		while(t-->0) {
+			String code = br.readLine();
+			String crypto = br.readLine();
+			for(i=0;i<code.length();i++) {
+				if(code.charAt(i)>='A'&&code.charAt(i)<='Z') {
+					sb.append(crypto.charAt(code.charAt(i)-'A'));
+				}else {
+					sb.append(code.charAt(i));
+				}
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
