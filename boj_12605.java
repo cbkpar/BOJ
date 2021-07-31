4fe0c8e5512ddf54dc4c37d60e6705368d51cd14
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-07-31 15:19:22 +0900

    12605번 : 단어순서 뒤집기

---
+++ boj_12605.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception{		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n,i;
+		n = Integer.parseInt(br.readLine());
+		for(i=1;i<=n;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+	        Stack<String> stack = new Stack<>(); 
+	        while(st.hasMoreTokens()) stack.add(st.nextToken());
+	        sb.append("Case #"+i+":");
+	        while(!stack.isEmpty()) sb.append(" "+stack.pop());
+	        sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
