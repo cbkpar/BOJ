77a0c9383b8a7cc11e3f418e8ab7af66aff4f463
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-08-28 14:52:00 +0900

    2435번 : 기상청 인턴 신현수

---
+++ boj_2435.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n,k,i,mx;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n+1];
+		st = new StringTokenizer(br.readLine());
+		for(i=1;i<=n;i++) arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
+		mx = Integer.MIN_VALUE;
+		for(i=k;i<=n;i++) if(arr[i]-arr[i-k]>mx) mx = arr[i]-arr[i-k];
+		System.out.println(mx);
+	}
+}
