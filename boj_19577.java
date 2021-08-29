36075c45c9222b9deeecb30abedb1a4b0b9c63c8
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-08-30 01:02:39 +0900

    19577번 : 수학은 재밌어

--- boj_19577.java
+++ boj_19577.java
@@ -6,44 +6,36 @@ public class Main {
 
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n,ans,i,j,sq,p,q,tsq;
+		int n,ans,i,sq,t;
 		n = Integer.parseInt(br.readLine());
 		sq = (int)Math.sqrt(n);
 		ans = Integer.MAX_VALUE;
 		for(i=1;i<=sq;i++) {
 			if(n%i==0) {
-				p = q = i;
-				tsq = (int)Math.sqrt(p);
-				for(j=2;j<=tsq;j++) {
-					if(p%j==0) {
-						q /= j;
-						q *= j-1;
-						while(p%j==0) p/=j;
-					}
-				}
-				if(p!=1) {
-					q /= p;
-					q *= p-1;
-				}
-				if(q*i==n) ans = Math.min(ans, i);
-			}
-			if(n%i==0) {
-				p = q = n/i;
-				tsq = (int)Math.sqrt(p);
-				for(j=2;j<=tsq;j++) {
-					if(p%j==0) {
-						q /= j;
-						q *= j-1;
-						while(p%j==0) p/=j;
-					}
-				}
-				if(p!=1) {
-					q /= p;
-					q *= p-1;
-				}
-				if(q*(n/i)==n) ans = Math.min(ans, (n/i));
+				t = eulerphi(i);
+				if(t*i==n) ans = Math.min(ans, i);
+				t = eulerphi(n/i);
+				if(t*(n/i)==n) ans = Math.min(ans, (n/i));
 			}
 		}
 		System.out.println(ans==Integer.MAX_VALUE?"-1":ans);
+	}
+	
+	public static int eulerphi(int n) {
+		int ans,sq,i;
+		ans = n;
+		sq = (int)Math.sqrt(ans);
+		for(i=2;i<=sq;i++) {
+			if(n%i==0) {
+				ans /= i;
+				ans *= i-1;
+				while(n%i==0) n/=i;
+			}
+		}
+		if(n!=1) {
+			ans /= n;
+			ans *= n-1;
+		}
+		return ans;
 	}
 }
