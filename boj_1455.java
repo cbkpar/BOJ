a46424d00a4a69bdf4bbb6d510c1cdfb7aafbe43
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-10-06 01:47:55 +0900

    1455번 : 뒤집기 II

---
+++ boj_1455.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[n + 1][m + 1];
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = str.charAt(j) - '0';
+            }
+        }
+        int ans = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                dp[i][j] = (dp[i + 1][j] + dp[i][j + 1] - dp[i + 1][j + 1] + 2) % 2;
+                if ((arr[i][j] + dp[i][j]) % 2 == 1) {
+                    dp[i][j] = (dp[i][j] + 1) % 2;
+                    ans++;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
