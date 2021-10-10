b3b9fd30c73a32c06cfc1d1fcfaa33573215904c
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-10-11 02:37:17 +0900

    6144번 : Charm Bracelet

---
+++ boj_6144.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + 1];
+        while (m-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            for (int i = n; i >= w; i--) {
+                arr[i] = Math.max(arr[i], arr[i - w] + d);
+            }
+        }
+        System.out.println(arr[n]);
+    }
+}
