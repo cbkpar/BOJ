ec099db48817525f1e1931411b2a36fd94869fb4
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-11-07 15:26:24 +0900

    1183번 : 약속

---
+++ boj_1183.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if (n % 2 == 1) {
+            System.out.println("1");
+            return;
+        }
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[i] = b - a;
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[n / 2] - arr[n / 2 - 1] + 1);
+    }
+}
