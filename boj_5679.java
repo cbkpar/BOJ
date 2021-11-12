2c298f9f8223f2079fa486eca3cf3d600f66272e
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-11-12 13:42:15 +0900

    5679번 : Hailstone Sequences

---
+++ boj_5679.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) break;
+            int mx = n;
+            while (n != 1) {
+                if (n % 2 == 0) {
+                    n /= 2;
+                } else {
+                    n = n * 3 + 1;
+                }
+                mx = Math.max(mx, n);
+            }
+            sb.append(mx + "\n");
+        }
+        System.out.println(sb);
+    }
+}
