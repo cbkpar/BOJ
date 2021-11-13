cfeea45c6942b28c6bb310f76d84b28c58e22ac7
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-11-14 02:25:42 +0900

    12875번 : 칙령

---
+++ boj_12875.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        int[][] dist = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < n; j++) {
+                if (str.charAt(j) == 'N') {
+                    dist[i][j] = 1000000000;
+                } else {
+                    dist[i][j] = 1;
+                }
+            }
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                ans = Math.max(ans, dist[i][j]);
+            }
+        }
+        System.out.println(ans == 1000000000 ? "-1" : ans * d);
+    }
+}
