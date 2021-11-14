ddd0bbe72169d3d215b8bb35176ea669c9962f21
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-11-14 12:37:30 +0900

    12738번 : 가장 긴 증가하는 부분 수열 3

---
+++ boj_12738.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(-1000000001);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int t = Integer.parseInt(st.nextToken());
+            if (t > arr.get(arr.size() - 1)) {
+                arr.add(t);
+            } else {
+                int left = 0;
+                int right = arr.size() - 1;
+                while (left < right) {
+                    int mid = (left + right) / 2;
+                    if (arr.get(mid) < t) {
+                        left = mid + 1;
+                    } else {
+                        right = mid;
+                    }
+                }
+                arr.set(left, t);
+            }
+        }
+        System.out.println(arr.size() - 1);
+    }
+}
