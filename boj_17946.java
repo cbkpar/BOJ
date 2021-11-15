93034c38b5ce7b3a0840223f315dfca073d6c132
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-11-16 03:25:39 +0900

    17946번 : 피자는 나눌 수록 커지잖아요

---
+++ boj_17946.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) sb.append("1\n");
+        System.out.println(sb);
+    }
+}
