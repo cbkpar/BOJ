f84189266ed9216abe41abc4468304cb4e6959b9
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-12-08 18:12:19 +0900

    17074번 : 정렬

---
+++ boj_17074.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+        int idx = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                cnt++;
+                idx = i;
+            }
+            if (cnt == 2) {
+                System.out.println("0");
+                return;
+            }
+        }
+        if (cnt == 0) {
+            System.out.println(n);
+            return;
+        }
+        int ans = 0;
+        if (idx == 0) {
+            ans++;
+        } else {
+            if (arr[idx - 1] <= arr[idx + 1]) ans++;
+        }
+        if (idx == n - 2) {
+            ans++;
+        } else {
+            if (arr[idx] <= arr[idx + 2]) ans++;
+        }
+        System.out.println(ans);
+    }
+}
