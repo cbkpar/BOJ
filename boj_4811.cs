ba795451cf753fe5333473c54da13e7330679977
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-12-19 20:34:48 +0900

    4811번 : 알약

---
+++ boj_4811.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text;
+class Program
+{
+  static void Main(String[] args)
+  {
+    StringBuilder sb = new StringBuilder();
+    long[,] dp = new long[31, 31];
+    for (int i = 0; i <= 30; i++) dp[0, i] = 1;
+    for (int i = 1; i <= 30; i++)
+    {
+      for (int j = i; j <= 30; j++)
+      {
+        dp[i, j] = dp[i - 1, j] + dp[i, j - 1];
+      }
+    }
+    while (true)
+    {
+      int n = int.Parse(Console.ReadLine());
+      if (n == 0) break;
+      sb.Append(dp[n, n] + "\n");
+    }
+    Console.WriteLine(sb);
+  }
+}
