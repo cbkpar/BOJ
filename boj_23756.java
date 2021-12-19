f2ef0d310204659dfb5b356af8d2844d086830e3
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-12-19 10:49:09 +0900

    23756번 : 노브 돌리기

---
+++ boj_23756.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int prev = Integer.parseInt(br.readLine());
+        int sum = 0;
+        while (n-- > 0) {
+            int next = Integer.parseInt(br.readLine());
+            int tmp = Math.abs(next - prev);
+            if (tmp > 180) tmp -= 180;
+            sum += tmp;
+            prev = next;
+        }
+        System.out.println(sum);
+    }
+}
