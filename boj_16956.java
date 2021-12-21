bfbf63725e4f01078af32b97158efa1cf4bef680
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-12-21 12:19:37 +0900

    16956번 : 늑대와 양

---
+++ boj_16956.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] dy = {-1, 0, 1, 0};
+        int[] dx = {0, -1, 0, 1};
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        char[][] map = new char[r][c];
+        for (int i = 0; i < r; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < c; j++) map[i][j] = str.charAt(j);
+        }
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (map[i][j] == 'S') {
+                    for (int k = 0; k < 4; k++) {
+                        int ny = i + dy[k];
+                        int nx = j + dx[k];
+                        if (ny < 0 || ny > r - 1 || nx < 0 || nx > c - 1) continue;
+                        if (map[ny][nx] == 'W') {
+                            System.out.println("0");
+                            return;
+                        }else if(map[ny][nx]=='.'){
+                            map[ny][nx] = 'D';
+                        }
+                    }
+                }
+            }
+        }
+        sb.append("1\n");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                sb.append(map[i][j]);
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
