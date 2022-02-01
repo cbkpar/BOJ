b8d9352b362cb2745ac4770a24a6e305d927a326
cbkpar <56503362+cbkpar@users.noreply.github.com>
2022-02-01 20:34:16 +0900

    2548번 : 대표 자연수

---
+++ boj_2548.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] presum = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        presum[0] = arr[0];
+        for (int i = 1; i < n; i++) presum[i] = presum[i - 1] + arr[i];
+        int ans = Integer.MAX_VALUE;
+        int mi = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            int tmp = (i + 1) * arr[i] - presum[i];
+            tmp += presum[n - 1] - presum[i] - (n - i - 1) * arr[i];
+            if (tmp < mi) {
+                mi = tmp;
+                ans = arr[i];
+            }
+        }
+        System.out.println(ans);
+    }
+}
