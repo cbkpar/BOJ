bc866a6f385e28c77c66663be222a71ceb3389ac
cbkpar <56503362+cbkpar@users.noreply.github.com>
2022-02-05 20:33:02 +0900

    15729번 : 방탈출

---
+++ boj_15729.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1) {
+                if (i + 1 < n) arr[i + 1] = (arr[i + 1] + 1) % 2;
+                if (i + 2 < n) arr[i + 2] = (arr[i + 2] + 1) % 2;
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
