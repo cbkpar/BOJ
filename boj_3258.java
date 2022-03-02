cdf7ca18da050ee030337e96999b3022574d70c8
cbkpar <56503362+cbkpar@users.noreply.github.com>
2022-03-02 12:40:55 +0900

    3258번 : 컴포트

--- boj_3258.java
+++ boj_3258.java
@@ -1,36 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
+    static int n, z, m;
+    static boolean[] obstacles;
+
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int z = Integer.parseInt(st.nextToken()) - 1;
-        int m = Integer.parseInt(st.nextToken());
-        boolean[] obstacles = new boolean[n];
+        n = Integer.parseInt(st.nextToken());
+        obstacles = new boolean[n];
+        z = Integer.parseInt(st.nextToken()) - 1;
+        m = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
         while (m-- > 0) obstacles[Integer.parseInt(st.nextToken()) - 1] = true;
+        System.out.println(findk());
+    }
+
+    private static int findk() {
         for (int step = 1; step < n; step++) {
-            Queue<Integer> q = new LinkedList<>();
+            int now = 0;
             boolean[] visited = new boolean[n];
             visited[0] = true;
-            q.add(0);
-            while (!q.isEmpty()) {
-                int p = q.poll();
-                if (p == z) {
-                    System.out.println(step);
-                    return;
-                }
-                p = (p + step) % n;
-                if (obstacles[p] || visited[p]) continue;
-                q.add(p);
+            while (true) {
+                if (now == z) return step;
+                now = (now + step) % n;
+                if (obstacles[now] || visited[now]) break;
             }
         }
+        return -1;
     }
 }
