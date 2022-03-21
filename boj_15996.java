b268b4740a803bb3c00cd803cb2cd9d12e11694a
cbkpar <56503362+cbkpar@users.noreply.github.com>
2022-03-21 11:19:56 +0900

    15996번 : 팩토리얼 나누기

---
+++ boj_15996.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long N = Long.parseLong(st.nextToken());
+        long A = Long.parseLong(st.nextToken());
+        long cnt = 0;
+        while (N >= A) {
+            cnt += N / A;
+            N /= A;
+        }
+        System.out.println(cnt);
+    }
+}
