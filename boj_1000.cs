ea53b37ff0ebba54f12c72228fa7cd7062389e39
cbkpar <56503362+cbkpar@users.noreply.github.com>
2022-04-30 23:11:17 +0900

    1000

--- boj_1000.cs
+++ boj_1000.cs
@@ -3,7 +3,7 @@ class Program
 {
   static void Main(String[] args)
   {
-    string[] input = Console.ReadLine().Split();
+    string[] input = Console.ReadLine().Split()
     Console.WriteLine(int.Parse(input[0]) + int.Parse(input[1]));
   }
 }
