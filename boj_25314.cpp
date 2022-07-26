c46f739aeaa25f98c6e524c9b38f854967a2bbfd
cbkpar <56503362+cbkpar@users.noreply.github.com>
2022-07-26 22:26:03 +0900

    25314번 : 코딩은 체육과목 입니다

---
+++ boj_25314.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int iN;
+	cin >> iN;
+	for (int i = 0; i < iN; i += 4)
+	{
+		cout << "long ";
+	}
+	cout << "int" << endl;
+	return 0;
+}
