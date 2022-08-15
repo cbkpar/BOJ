9d53e7b8b8b37c9f99e256c68d06c0f31898da7e
cbkpar <56503362+cbkpar@users.noreply.github.com>
2022-08-15 23:30:06 +0900

    24479번 : 알고리즘 수업 - 깊이 우선 탐색 1

---
+++ boj_24479.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static ArrayList<Integer>[] arrNode;
+    static boolean[] bVisited;
+    static int[] arrCount;
+    static int iCount = 0;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int iN = Integer.parseInt(st.nextToken());
+        int iM = Integer.parseInt(st.nextToken());
+        int iR = Integer.parseInt(st.nextToken());
+        arrNode = new ArrayList[iN + 1];
+        for (int i = 1; i <= iN; ++i) {
+            arrNode[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < iM; ++i) {
+            st = new StringTokenizer(br.readLine());
+            int iS = Integer.parseInt(st.nextToken());
+            int iE = Integer.parseInt(st.nextToken());
+            arrNode[iS].add(iE);
+            arrNode[iE].add(iS);
+        }
+        for (int i = 1; i <= iN; ++i) {
+            Collections.sort(arrNode[i]);
+        }
+        bVisited = new boolean[iN + 1];
+        arrCount = new int[iN + 1];
+        bVisited[iR] = true;
+        dfs(iR);
+        for (int i = 1; i <= iN; ++i) {
+            sb.append(arrCount[i] + "\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void dfs(int iN) {
+        arrCount[iN] = ++iCount;
+        for (int iNextPos : arrNode[iN]) {
+            if (!bVisited[iNextPos]) {
+                bVisited[iNextPos] = true;
+                dfs(iNextPos);
+            }
+        }
+    }
+}
