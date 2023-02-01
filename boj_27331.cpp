de00598bf3be40abcf01d72b71abc9ca3e230eca
cbkpar <56503362+cbkpar@users.noreply.github.com>
2023-02-01 23:28:58 +0900

    27331번 : 2 桁の整数 (Two-digit Integer)

---
+++ boj_27331.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int iA, iB;
+
+	cin >> iA >> iB;
+	cout << (iA * 10 + iB) << "\n";
+
+	return 0;
+}
