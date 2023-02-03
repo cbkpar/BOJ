4f565338f68dd65a08b23c85e7f99e103da4836e
cbkpar <56503362+cbkpar@users.noreply.github.com>
2023-02-03 23:47:31 +0900

    27327번 : 時間 (Hour)

---
+++ boj_27327.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int iN;
+	cin >> iN;
+
+	cout << (iN * 24) << "\n";
+
+	return 0;
+}
