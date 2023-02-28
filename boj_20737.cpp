d8f6037d336ab936ad8d75b7f976765d9cec4667
cbkpar <56503362+cbkpar@users.noreply.github.com>
2023-03-01 03:18:39 +0900

    20737번 : Sanic

---
+++ boj_20737.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	double dR;
+	cin >> dR;
+
+	cout << (dR - 1.0) << "\n";
+
+	return 0;
+}
