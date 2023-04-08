614f7344ed0461fcaa78bb11e8094628c342df66
cbkpar <56503362+cbkpar@users.noreply.github.com>
2023-04-09 03:18:07 +0900

    26027번 : Disc District

---
+++ boj_26027.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int iR;
+	cin >> iR;
+	cout << iR << " 1\n";
+
+	return 0;
+}
