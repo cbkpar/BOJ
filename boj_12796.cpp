b8d91d604b7311f8c632cca92846a7198844c677
cbkpar <56503362+cbkpar@users.noreply.github.com>
2023-06-04 14:16:42 +0900

    12796번 : 나의 행렬곱셈 답사기
    adhoc

---
+++ boj_12796.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int iN;
+	cin >> iN;
+
+	cout << "3\n";
+	cout << "1 1 " << (iN + 1) << " 1\n";
+	return 0;
+}
