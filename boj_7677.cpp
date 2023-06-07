64850f5f6f752ad81cba60516c8340d9a1f92d35
cbkpar <56503362+cbkpar@users.noreply.github.com>
2023-06-07 14:35:49 +0900

    7677번 : Fibonacci
    fibonacci

---
+++ boj_7677.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while(true)
+	{
+		long long lNumber;
+		cin >> lNumber;
+		
+		if (lNumber == -1) break;
+		if (lNumber == 0)
+		{
+			cout << "0\n";
+			continue;
+		}
+
+		--lNumber;
+		long long matAnswer[2][2] = { {1,0},{0,1} };
+		long long matDouble[2][2] = { {1,1},{1,0} };
+
+		while (lNumber)
+		{
+			if (lNumber % 2 == 1)
+			{
+				long long matTemp[2][2] = { 0, };
+				for (int i = 0; i < 2; ++i)
+				{
+					for (int j = 0; j < 2; ++j)
+					{
+						for (int k = 0; k < 2; ++k)
+						{
+							matTemp[i][j] += matDouble[i][k] * matAnswer[k][j];
+						}
+					}
+				}
+				for (int i = 0; i < 2; ++i)
+				{
+					for (int j = 0; j < 2; ++j)
+					{
+						matAnswer[i][j] = (matTemp[i][j]) % 10000;
+					}
+				}
+			}
+			long long matTemp[2][2] = { 0, };
+			for (int i = 0; i < 2; ++i)
+			{
+				for (int j = 0; j < 2; ++j)
+				{
+					for (int k = 0; k < 2; ++k)
+					{
+						matTemp[i][j] += matDouble[i][k] * matDouble[k][j];
+					}
+				}
+			}
+			for (int i = 0; i < 2; ++i)
+			{
+				for (int j = 0; j < 2; ++j)
+				{
+					matDouble[i][j] = (matTemp[i][j]) % 10000;
+				}
+			}
+			lNumber /= 2;
+		}
+		cout << (matAnswer[1][0] + matAnswer[1][1]) % 10000 << "\n";
+	}
+	return 0;
+}
