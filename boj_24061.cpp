ae7ffd266c217253a579e899db1d9de38eb91026
cbkpar <56503362+cbkpar@users.noreply.github.com>
2023-07-01 14:00:24 +0900

    24061번 : 알고리즘 수업 - 병합 정렬 2
    Merge sort

---
+++ boj_24061.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int iN, iK, iAns;
+
+void Merge(vector<int>& vecNum, int p, int q, int r)
+{
+	int i = p;
+	int j = q + 1;
+	int t = 0;
+	vector<int> vecTemp(r - p + 1, 0);
+
+	while (i <= q && j <= r)
+	{
+		if (vecNum[i] <= vecNum[j])
+		{
+			vecTemp[t++] = vecNum[i++];
+		}
+		else
+		{
+			vecTemp[t++] = vecNum[j++];
+		}
+	}
+	while (i <= q) vecTemp[t++] = vecNum[i++];
+	while (j <= r) vecTemp[t++] = vecNum[j++];
+	
+	i = p;
+	t = 0;
+	while (i <= r)
+	{
+		vecNum[i++] = vecTemp[t++];
+		if (--iK == 0)
+		{
+			iAns = 0;
+			for (int k = 0; k < iN; ++k)
+			{
+				cout << vecNum[k] << (k == iN - 1 ? "\n" : " ");
+			}
+		}
+	}
+}
+
+void MergeSort(vector<int>& vecNum, int p, int r)
+{
+	if (p < r)
+	{
+		int q = (p + r) >> 1;
+		MergeSort(vecNum, p, q);
+		MergeSort(vecNum, q + 1, r);
+		Merge(vecNum, p, q, r);
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> iN >> iK;
+	iAns = -1;
+
+	vector<int> vecNum(iN, 0);
+	for (int i = 0; i < iN; ++i) cin >> vecNum[i];
+	
+	MergeSort(vecNum, 0, iN - 1);
+	if(iAns == -1) cout << "-1\n";
+
+	return 0;
+}
