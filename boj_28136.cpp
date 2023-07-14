7dc17550657c0abbb77ca844a14866e3ebbddacb
cbkpar <56503362+cbkpar@users.noreply.github.com>
2023-07-14 12:24:33 +0900

    28136번 : 원, 탁!

---
+++ boj_28136.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int iN;
+	cin >> iN;
+	vector<int> vecNum(iN, 0);
+	for(int i=0;i<iN;++i) cin >> vecNum[i];
+	
+	int iAns = 0;
+	for(int i=0;i<iN;++i)
+	{
+	    if(vecNum[i] >= vecNum[(i+1)%iN])
+	    {
+	        ++iAns;
+	    }
+	}
+	
+	cout << iAns << "\n";
+	return 0;
+}
