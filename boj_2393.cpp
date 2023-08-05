029730991997e1915dce625a525054a6c61fc482
cbkpar <56503362+cbkpar@users.noreply.github.com>
2023-08-05 13:05:20 +0900

    2393번 : Rook

---
+++ boj_2393.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cout << "  ___  ___  ___" << "\n";
+	cout << "  | |__| |__| |" << "\n";
+	cout << "  |           |" << "\n";
+	cout << "   \\_________/" << "\n";
+	cout << "    \\_______/" << "\n";
+	cout << "     |     |" << "\n";
+	cout << "     |     |" << "\n";
+	cout << "     |     |" << "\n";
+	cout << "     |     |" << "\n";
+	cout << "     |_____|" << "\n";
+	cout << "  __/       \\__" << "\n";
+	cout << " /             \\" << "\n";
+	cout << "/_______________\\" << "\n";
+
+	return 0;
+}
