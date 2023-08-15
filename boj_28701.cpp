160e7f3920d6f309e39b02a974ceefb31ad704ce
cbkpar <56503362+cbkpar@users.noreply.github.com>
2023-08-15 09:14:54 +0900

    28701번 : 세제곱의 합

---
+++ boj_28701.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	long long lN;
+	cin >> lN;
+
+	cout << (lN) * (lN + 1) / 2 << "\n";
+	cout << (lN) * (lN + 1) / 2 * (lN) * (lN + 1) / 2 << "\n";
+	cout << (lN) * (lN + 1) / 2 * (lN) * (lN + 1) / 2 << "\n";
+
+	return 0;
+}
