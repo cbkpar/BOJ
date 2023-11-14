b5b05c116530bd5745e5423487e9eecf0560df21
cbkpar <56503362+cbkpar@users.noreply.github.com>
2023-11-14 21:38:56 +0900

    30455번 : 이제는 더 이상 물러날 곳이 없다
    game theory

---
+++ boj_30455.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int iN;
+	cin >> iN;
+
+	if (iN % 2 == 1)
+	{
+		cout << "Goose" << "\n";
+	}
+	else
+	{
+		cout << "Duck" << "\n";
+	}
+	return 0;
+}
