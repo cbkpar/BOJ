4430ec98c54ee0a671859bdf6e8fb416134b8851
cbkpar <56503362+cbkpar@users.noreply.github.com>
2024-02-18 23:31:12 +0900

    31429번 : SUAPC 2023 Summer

---
+++ boj_31429.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int iN;
+	cin >> iN;
+
+	if (iN == 1) cout << "12 1600" << "\n";
+	if (iN == 2) cout << "11 894" << "\n";
+	if (iN == 3) cout << "11 1327" << "\n";
+	if (iN == 4) cout << "10 1311" << "\n";
+	if (iN == 5) cout << "9 1004" << "\n";
+	if (iN == 6) cout << "9 1178" << "\n";
+	if (iN == 7) cout << "9 1357" << "\n";
+	if (iN == 8) cout << "8 837" << "\n";
+	if (iN == 9) cout << "7 1055" << "\n";
+	if (iN == 10) cout << "6 556" << "\n";
+	if (iN == 11) cout << "6 773" << "\n";
+
+	return 0;
+}
