408b88a6a8dc60e67f51a5f1869f1191da7f198c
cbkpar <56503362+cbkpar@users.noreply.github.com>
2024-03-07 07:41:56 +0900

    31281번 : ЗЛАТНАТА СРЕДА

---
+++ boj_31281.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	vector<long long> vecNum(3, 0);
+	for (int i = 0;i < 3;++i) cin >> vecNum[i];
+
+	sort(vecNum.begin(), vecNum.end());
+
+	cout << vecNum[1] << "\n";
+	return 0;
+}
