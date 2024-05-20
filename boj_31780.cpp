87bc5c4927f3188a93193969956a3b159baf4fad
cbkpar <56503362+cbkpar@users.noreply.github.com>
2024-05-20 20:53:38 +0900

    31780번 : 불사조
    adhoc

---
+++ boj_31780.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	long long lX;
+	long long lM;
+	cin >> lX >> lM;
+
+	cout << lX*(lM+1)<< "\n";
+	return 0;
+}
