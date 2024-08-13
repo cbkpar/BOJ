11c84afa7ff28798d1b5e09352aa503cbb1a141c
cbkpar <56503362+cbkpar@users.noreply.github.com>
2024-08-14 07:20:06 +0900

    31746번 : SciComLove (2024)

---
+++ boj_31746.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int iN;
+	cin >> iN;
+
+	if (iN % 2 == 1)
+	{
+		cout << "evoLmoCicS" << "\n";
+	}
+	else
+	{
+		cout << "SciComLove" << "\n";
+	}
+	return 0;
+}
