f490279d728f4e9e4dfee1de36d44a353f912262
cbkpar <56503362+cbkpar@users.noreply.github.com>
2024-09-06 18:09:53 +0900

    32154번 : SUAPC 2024 Winter

---
+++ boj_32154.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int iN;
+	cin >> iN;
+
+	if(iN == 1)
+	{
+		cout << "11" << "\n";
+		cout << "A B C D E F G H J L M" << "\n";
+	}
+	if(iN == 2)
+	{
+		cout << "9" << "\n";
+		cout << "A C E F G H I L M" << "\n";
+	}
+	if(iN == 3)
+	{
+		cout << "9" << "\n";
+		cout << "A C E F G H I L M" << "\n";
+	}
+	if(iN == 4)
+	{
+		cout << "9" << "\n";
+		cout << "A B C E F G H L M" << "\n";
+	}
+	if(iN == 5)
+	{
+		cout << "8" << "\n";
+		cout << "A C E F G H L M" << "\n";
+	}
+	if(iN == 6)
+	{
+		cout << "8" << "\n";
+		cout << "A C E F G H L M" << "\n";
+	}
+	if(iN == 7)
+	{
+		cout << "8" << "\n";
+		cout << "A C E F G H L M" << "\n";
+	}
+	if(iN == 8)
+	{
+		cout << "8" << "\n";
+		cout << "A C E F G H L M" << "\n";
+	}
+	if(iN == 9)
+	{
+		cout << "8" << "\n";
+		cout << "A C E F G H L M" << "\n";
+	}
+	if(iN == 10)
+	{
+		cout << "8" << "\n";
+		cout << "A B C F G H L M" << "\n";
+	}
+	return 0;
+}
