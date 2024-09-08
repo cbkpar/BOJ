8338aac27a0160b1faa7825caf98f8122237d35d
cbkpar <56503362+cbkpar@users.noreply.github.com>
2024-09-08 23:21:57 +0900

    32215번 : 코드마스터 2024

---
+++ boj_32215.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int iN, iM, iK;
+	cin >> iN >> iM >> iK;
+
+	cout << (iM * (iK + 1)) << "\n";
+	return 0;
+}
