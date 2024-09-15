8cc735b92597a2beb2504066b4a76d1f5dcbdfa4
cbkpar <56503362+cbkpar@users.noreply.github.com>
2024-09-15 23:25:27 +0900

    32246번 : 빙고 막기
    adhoc

---
+++ boj_32246.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int iN;
+	cin >> iN;
+
+	int iAns = (iN == 2 ? 3 : iN);
+	cout << iAns << "\n";
+	return 0;
+}
