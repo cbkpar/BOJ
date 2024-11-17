ac3f82862d3f5079e1f3e213c5a37b56f9486a26
cbkpar <56503362+cbkpar@users.noreply.github.com>
2024-11-17 23:38:31 +0900

    32652번 : 아카라카 2

---
+++ boj_32652.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int iN;
+	cin >> iN;
+
+	cout << "AKA";
+	for (int i = 0; i < iN; ++i)
+	{
+		cout << "RAKA";
+	}
+	cout << "\n";
+	return 0;
+}
