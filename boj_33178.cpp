26646ede2da884f138d14268ec6e7c1e8b6aee62
cbkpar <56503362+cbkpar@users.noreply.github.com>
2025-01-15 21:52:50 +0900

    33178번 : Micromasters

---
+++ boj_33178.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int iN;
+	cin >> iN;
+
+	cout << (iN / 10) << "\n";
+	return 0;
+}
