4bdda236790c6dd8651be5d08b881680a76c9429
cbkpar <56503362+cbkpar@users.noreply.github.com>
2025-02-03 21:44:23 +0900

    33161번 : 鉛筆 2 (Pencils 2)

---
+++ boj_33161.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int iN;
+	cin >> iN;
+
+	cout << (iN / 5) << "\n";
+
+	return 0;
+}
