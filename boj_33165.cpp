ac0222ce06aeb6c4097234e7590d42a2333073b2
cbkpar <56503362+cbkpar@users.noreply.github.com>
2025-02-19 16:25:19 +0900

    33165번 : 徒競走 (Footrace)

---
+++ boj_33165.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	long long lA, lB;
+	cin >> lA >> lB;
+    
+    cout << (lA * lB) << "\n";
+	return 0;
+}
