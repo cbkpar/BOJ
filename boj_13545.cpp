80f01fbd50a6c79b5a3cf2af599c8fb31faebcd5
cbkpar <56503362+cbkpar@users.noreply.github.com>
2025-05-14 23:16:01 +0900

    13545번 : 수열과 쿼리 0
    Mo's Algorithm, Sqrt Decomposition, presum

---
+++ boj_13545.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <math.h>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int iN;
+	cin >> iN;
+
+	int iSqrt = (int)sqrt(iN * 2);
+
+	vector<int> vecNum(iN + 1, 0);
+	for (int i = 1;i <= iN;++i)
+	{
+		cin >> vecNum[i];
+	}
+
+	vector<int> vecPresum(iN + 1, 0);
+	for (int i = 1;i <= iN;++i)
+	{
+		vecPresum[i] = vecPresum[i - 1] + vecNum[i];
+	}
+
+	for (int i = 0;i <= iN;++i)
+	{
+		vecPresum[i] += iN;
+	}
+
+	vector<tuple<int, int, int>> vecQuery;
+
+	int iM;
+	cin >> iM;
+	for (int t = 0;t < iM;++t)
+	{
+		int iL, iR;
+		cin >> iL >> iR;
+		vecQuery.push_back(make_tuple(iL - 1, iR, t));
+	}
+
+	sort(vecQuery.begin(), vecQuery.end(), [&](const tuple<int, int, int>& o1, const tuple<int, int, int>& o2)->bool
+		{
+			if (get<0>(o1) / iSqrt == get<0>(o2) / iSqrt) return get<1>(o1) < get<1>(o2);
+			return get<0>(o1) < get<0>(o2);
+		});
+
+
+	vector<list<int>> vecList(iN * 2 + 1, list<int>());
+
+	int iSize = ((iN * 2) / iSqrt) + 1;
+	vector<int> vecCount(iN * 2 + 1, 0);
+	vector<int> vecBucket(iSize + 1, 0);
+
+
+	vector<int> vecAns(iM, 0);
+	int iS = 0;
+	int iE = 0;
+
+	for (int t = 0;t < iM;++t)
+	{
+		int iL = get<0>(vecQuery[t]);
+		int iR = get<1>(vecQuery[t]);
+		int iIndex = get<2>(vecQuery[t]);
+
+		while (iE < iR)
+		{
+			++iE;
+			if (vecList[vecPresum[iE]].size() > 0)
+			{
+				int iBefore = vecList[vecPresum[iE]].back() - vecList[vecPresum[iE]].front();
+				--vecBucket[iBefore / iSqrt];
+				--vecCount[iBefore];
+			}
+			vecList[vecPresum[iE]].push_back(iE);
+			int iAfter = vecList[vecPresum[iE]].back() - vecList[vecPresum[iE]].front();
+			++vecBucket[iAfter / iSqrt];
+			++vecCount[iAfter];
+		}
+		while (iS >= iL)
+		{
+			if (vecList[vecPresum[iS]].size() > 0)
+			{
+				int iBefore = vecList[vecPresum[iS]].back() - vecList[vecPresum[iS]].front();
+				--vecBucket[iBefore / iSqrt];
+				--vecCount[iBefore];
+			}
+			vecList[vecPresum[iS]].push_front(iS);
+			int iAfter = vecList[vecPresum[iS]].back() - vecList[vecPresum[iS]].front();
+			++vecBucket[iAfter / iSqrt];
+			++vecCount[iAfter];
+			--iS;
+		}
+		while (iE > iR)
+		{
+			int iBefore = vecList[vecPresum[iE]].back() - vecList[vecPresum[iE]].front();
+			--vecBucket[iBefore / iSqrt];
+			--vecCount[iBefore];
+			vecList[vecPresum[iE]].pop_back();
+			if (vecList[vecPresum[iE]].size() > 0)
+			{
+				int iAfter = vecList[vecPresum[iE]].back() - vecList[vecPresum[iE]].front();
+				++vecBucket[iAfter / iSqrt];
+				++vecCount[iAfter];
+			}
+			--iE;
+		}
+		while (iS + 1 < iL)
+		{
+			++iS;
+			int iBefore = vecList[vecPresum[iS]].back() - vecList[vecPresum[iS]].front();
+			--vecBucket[iBefore / iSqrt];
+			--vecCount[iBefore];
+			vecList[vecPresum[iS]].pop_front();
+			if (vecList[vecPresum[iS]].size() > 0)
+			{
+				int iAfter = vecList[vecPresum[iS]].back() - vecList[vecPresum[iS]].front();
+				++vecBucket[iAfter / iSqrt];
+				++vecCount[iAfter];
+			}
+		}
+
+		for (int i = iSize;i >= 0;--i)
+		{
+			if (vecBucket[i] > 0)
+			{
+				for (int j = iSqrt - 1;j >= 0;--j)
+				{
+					if (i * iSqrt + j > iN * 2) continue;
+					if (vecCount[i * iSqrt + j] > 0)
+					{
+						vecAns[iIndex] = i * iSqrt + j;
+						break;
+					}
+				}
+				break;
+			}
+		}
+	}
+
+	for (int i = 0;i < iM;++i)
+	{
+		cout << vecAns[i] << "\n";
+	}
+
+	return 0;
+}
