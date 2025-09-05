c6414ae544a5af614b0b4f8b307dcf682e300b8c
cbkpar <56503362+cbkpar@users.noreply.github.com>
2025-09-05 10:43:22 +0900

    34161번 : OO0OO

---
+++ boj_34161.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	for (int i = 0; i < 10000; ++i)
+	{
+		cout << "-1" << "\n";
+	}
+
+	return 0;
+}
