03eb73a0d49508f0661d55f9152352b9e45bcc31
cbkpar <56503362+cbkpar@users.noreply.github.com>
2025-09-09 21:45:06 +0900

    34281번 : Class Photo

---
+++ boj_34281.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int iW, iL;
+	cin >> iW >> iL;
+	int iAns = iW * iL;
+
+	cout << iAns << "\n";
+	return 0;
+}
