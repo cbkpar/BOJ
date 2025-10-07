8a473ec0d78f5be458f9998e2892d95efcb6ce72
cbkpar <56503362+cbkpar@users.noreply.github.com>
2025-10-07 22:52:51 +0900

    34424번 : Flatter Land

---
+++ boj_34424.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    int iA, iB;
+    cin >> iA >> iB;
+    cout << (iA-1)*iB << "\n";
+    
+	return 0;
+}
