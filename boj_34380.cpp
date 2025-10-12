390a2f6e76667bbfac5010ec8be4b4c1ab01cdea
cbkpar <56503362+cbkpar@users.noreply.github.com>
2025-10-12 23:43:46 +0900

    34380번 : Blaster's Dash

---
+++ boj_34380.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int iM, iV, iN, iK;
+	cin >> iM >> iV >> iN >> iK;
+
+	int iAns = (iN + 20) * 2;
+	cout << iAns << "\n";
+	return 0;
+}
