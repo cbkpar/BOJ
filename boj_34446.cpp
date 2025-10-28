69e93a05f133746d612e17c30f50bc7877656b8c
cbkpar <56503362+cbkpar@users.noreply.github.com>
2025-10-29 00:19:36 +0900

    34446번 : E-Days Ore Cart Pull

---
+++ boj_34446.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int iN;
+	cin >> iN;
+	cout << (iN * 2) << "\n";
+
+	return 0;
+}
