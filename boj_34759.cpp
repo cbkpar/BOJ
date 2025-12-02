f83a6a9dcc57962a5b03b018704eaee23bcfeeaa
cbkpar <56503362+cbkpar@users.noreply.github.com>
2025-12-02 09:05:09 +0900

    34759번 : 얼룩말과 사자

---
+++ boj_34759.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	unsigned long long lN;
+	cin >> lN;
+	cout << (lN * 2) << "\n";
+	return 0;
+}
