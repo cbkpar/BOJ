ed33548b9ecdd061bec491fea3c3997b9c839a16
cbkpar <56503362+cbkpar@users.noreply.github.com>
2026-01-28 21:12:26 +0900

    34667번 : 가희와 철도역

---
+++ boj_34667.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	string strA, strB;
+	cin >> strA >> strB;
+
+	cout << strA[0] << strA << "\n";
+	return 0;
+}
