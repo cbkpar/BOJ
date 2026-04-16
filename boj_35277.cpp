e3f2081a85afe35d51dd8b2b3fce9dfe1a7e1fe7
cbkpar <56503362+cbkpar@users.noreply.github.com>
2026-04-16 23:42:38 +0900

    35277번 : 유림이와 하람이의 두쫀쿠 대작전

---
+++ boj_35277.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	int iN;
+	cin >> iN;
+
+	int iAns = iN / 2000;
+	cout << iAns << "\n";
+	return 0;
+}
